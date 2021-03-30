b157b4c495fd41f57b9c94546d03598e6e85dda8
TheCoumanKing <thecouman@gmail.com>
2021-03-30 14:18:12 +0530

    jhj

--- README.md
+++
@@ -1,5 +0,0 @@
-# node-mongo-jwt-refresh-tokens-api
-
-Node.js + MongoDB API - JWT Authentication with Refresh Tokens
-
-For documentation and tutorial see https://jasonwatmore.com/post/2020/06/17/nodejs-mongodb-api-jwt-authentication-with-refresh-tokens
--- _helpers/swagger.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-module.exports = router;
